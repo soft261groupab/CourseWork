b4fb10926eeb2e2c50ce8ea1235c8f859bce0a02
(s) Harry Fresco <harry.fresco@students.plymouth.ac.uk>
2019-05-28 19:39:51 +0100

    Commented Part A

--- Coursework_May_18-19_Part_A/main.cpp
+++ Coursework_May_18-19_Part_A/main.cpp
@@ -23,6 +23,7 @@ float inputFrequency = 10;
 bool check = false;
 float bufferArray[20];
 int arrayCounter = 0;
+int secondCounter = 0;
 float sum = 0;
 float averageBuffer = 0;
 char inputPassword[20];
@@ -30,17 +31,21 @@ char inputChange[20];
 
 
 void arrayAverage(){
-	if(arrayCounter == inputFrequency){
-		arrayCounter=0;
-				printf("%5.3f\n", averageBuffer);
-			}
+	//If data is taken the same number of times as the frequency
+	// then 1 second has passed, so the average is calculated
+	if(secondCounter == inputFrequency){
+		secondCounter=0;
+				printf("Average input = %5.3f\n", averageBuffer);
+			
+		//Get the sum of array
     for(int i = 0; i<sizeof(bufferArray); i++){
         sum = sum + bufferArray[i];
     }
+		//Divide sum by size
     averageBuffer = sum /20;
     
 		sum = 0;
-		
+	}
 }
 	
 int main() {
@@ -49,28 +54,44 @@ int main() {
 		
 		//Read ADC
 		fVin = AIN;
+		//Add to circular buffer
 		bufferArray[arrayCounter] = fVin;
+		//If array is full, set counter back to 0
+		if(arrayCounter== 19){
+			arrayCounter=0;
+		}
 		
+		//Increment counters
     arrayCounter++;
+		secondCounter++;
+		
+		//Go to the arrayFuntion to check if second has passed
+		//	and also calculate average.
     arrayAverage();
         
 		
-		//printf("Analog input = %5.3f\n", fVin);
-		binaryOutput = 2;
-		//Wait
+		printf("Analog input = %5.3f\n", fVin);
+		//Turn on green LED
+		binaryOutput = 1;
+		//Wait 1/frequency seconds
 		wait(sampleStr);
+		//Turn off green LED
 		binaryOutput = 0;
 		
-		
+	//If switch is pressed...
 	if(SW2 == 1){
-	
-			binaryOutput = 1;
+			//Turn on red LED
+			binaryOutput = 4;
 			check = false;
 
 					while(check == false){
+						//User enters frequency
 						printf("Enter Frequency: ");
 						pc.scanf("%f", inputFrequency);
+						
+					//Checks frequency between 5 and 100Hz	
 					if(inputFrequency <= 100 && inputFrequency >= 5){
+						//Changes from hertz to seconds
 						sampleStr = 1 / inputFrequency;
 						check = true;
 					}
